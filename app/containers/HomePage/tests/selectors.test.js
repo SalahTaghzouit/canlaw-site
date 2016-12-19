8156f78c2db2bf97e5a543bc30ece8df0ef2aa16
Salah Taghzouit <salah.taghzouit@gmail.com>
2016-12-19 18:07:52 +0400

    tests (dummy)

---
+++ app/containers/HomePage/tests/selectors.test.js
@@ -0,0 +1,10 @@
+// import { fromJS } from 'immutable';
+// import { makeSelectQuoteRequestDomain } from '../selectors';
+
+// const selector = makeSelectQuoteRequestDomain();
+
+describe('makeSelectHomeDomain', () => {
+  it('Expect to have unit tests specified', () => {
+    expect('Test case').toEqual(false);
+  });
+});
