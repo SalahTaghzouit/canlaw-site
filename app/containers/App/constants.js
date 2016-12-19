e298a2a4eaff399cb4f831ba3647cf38e43baef7
Aladin Bouzerd <aladin.bouzerd@gmail.com>
2016-12-19 11:19:17 +0400

    added algolia

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -2,11 +2,11 @@
  * AppConstants
  * Each action has a corresponding type, which the reducer knows and picks up on.
  * To avoid weird typos between the reducer and the actions, we save them as
- * constants here. We prefix them with 'yourproject/YourComponent' so we avoid
+ * constants here. We prefix them with 'canlaw/ComponentName' so we avoid
  * reducers accidentally picking up actions they shouldn't.
  *
  * Follow this format:
- * export const YOUR_ACTION_CONSTANT = 'yourproject/YourContainer/YOUR_ACTION_CONSTANT';
+ * export const ACTION_CONSTANT = 'canlaw/ContainerName/ACTION_CONSTANT';
  */
 
 export const DEFAULT_LOCALE = 'en';
