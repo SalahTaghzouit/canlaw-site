e641b3689aba8178e49a3c93f7b2758770482c66
Salah Taghzouit <salah.taghzouit@gmail.com>
2016-12-20 21:37:58 +0400

    (fix) message was not displayed properly

--- app/components/SearchBox/index.js
+++ app/components/SearchBox/index.js
@@ -5,23 +5,24 @@
  */
 import React from 'react';
 import { connect } from 'react-algoliasearch-helper';
-import { FormattedMessage } from 'react-intl';
+import { injectIntl } from 'react-intl';
 import messages from './messages';
 
-export function SearchBox({ helper, initialText }) {
+export function SearchBox({ helper, initialText, intl }) {
   return (
     <input
-      placeholder={<FormattedMessage {...messages.pleaceholder} />}
+      placeholder={intl.formatMessage({ ...messages.pleaceholder })}
       autoFocus
-      value={initialText}
+      defaultValue={initialText}
       onChange={(e) => helper.setQuery(e.target.value).search()}
     />
   );
 }
 
 SearchBox.propTypes = {
-  helper: React.PropTypes.object,
+  helper: React.PropTypes.object.isRequired,
   initialText: React.PropTypes.string,
+  intl: React.PropTypes.object.isRequired,
 };
 
-export default connect()(SearchBox);
+export default connect()(injectIntl(SearchBox));
