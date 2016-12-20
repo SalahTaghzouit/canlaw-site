f8a5f22d9a9cfb6513dbc6bdae16807b9cd17b9d
Salah Taghzouit <salah.taghzouit@gmail.com>
2016-12-20 21:40:43 +0400

    (new) added questions texts (business category only)

--- app/containers/Questions/messages.js
+++ app/containers/Questions/messages.js
@@ -6,8 +6,68 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  header: {
-    id: 'app.containers.Questions.header',
-    defaultMessage: 'This is Questions container !',
+  parties: {
+    id: 'canlaw.containers.Questions.parties',
+    defaultMessage: 'How many parties are involved?',
+  },
+
+  businessNature: {
+    id: 'canlaw.containers.Questions.businessNature',
+    defaultMessage: 'What is the nature of your business?',
+  },
+
+  businessNatureExample: {
+    id: 'canlaw.containers.Questions.businessNatureExample',
+    defaultMessage: 'Business Nature example',
+  },
+
+  areasOfConcern: {
+    id: 'canlaw.containers.Questions.areasOfConcern',
+    defaultMessage: 'What are the areas of concern you want addressed within this agreement?',
+  },
+
+  areasOfConcernExample: {
+    id: 'canlaw.containers.Questions.areasOfConcernExample',
+    defaultMessage: 'Example of concerns',
+  },
+
+  whatDoYouHope: {
+    id: 'canlaw.containers.Questions.whatDoYouHope',
+    defaultMessage: 'What do you hope to achieve with this document?',
+  },
+
+  whatDoYouHopeExample: {
+    id: 'canlaw.containers.Questions.whatDoYouHopeExample',
+    defaultMessage: 'Example text',
+  },
+
+  howManyOwners: {
+    id: 'canlaw.containers.Questions.howManyOwners',
+    defaultMessage: 'How many owners will this company have?',
+  },
+
+  listOwners: {
+    id: 'canlaw.containers.Questions.listOwners',
+    defaultMessage: 'List the owners, their roles, and respective percentage of ownership',
+  },
+
+  nonActiveInvestors: {
+    id: 'canlaw.containers.Questions.nonActiveInvestors',
+    defaultMessage: 'Are there investors or partners who are not involved in the day-to-day management of the business?',
+  },
+
+  howsItFunded: {
+    id: 'canlaw.containers.Questions.howsItFunded',
+    defaultMessage: 'Which of the below will be used to fund the business?',
+  },
+
+  howBigIsDocument: {
+    id: 'canlaw.containers.Questions.howBigIsDocument',
+    defaultMessage: 'How long is the document?',
+  },
+
+  purposeOfDocument: {
+    id: 'canlaw.containers.Questions.purposeOfDocument',
+    defaultMessage: 'What is the purpose of the document?',
   },
 });
