1eda530a175a7bb1983a137b96e658877e4f3a44
Salah Taghzouit <salah.taghzouit@gmail.com>
2016-12-20 21:37:27 +0400

    (new) question control

---
+++ app/components/QuestionControl/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import QuestionControl from '../index';
+
+describe('<QuestionControl />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
