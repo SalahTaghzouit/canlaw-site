5e4cfc07edbfd4ce243a955fbaf6a73cedce49c8
Salah Taghzouit <salah.taghzouit@gmail.com>
2016-12-21 10:53:19 +0400

    (fix) mutating the field while rendering moved to after render hook

--- app/components/QuestionControl/index.js
+++ app/components/QuestionControl/index.js
@@ -2,23 +2,30 @@ import React, { PropTypes } from 'react';
 import Label from './Label';
 import Input from './Input';
 
-function Control(props) {
-  return (
-    <div>
-      <Label>{props.question}</Label>
-      <Input
-        type={props.type || 'text'}
-        placeholder={props.placeholder && props.placeholder}
-        required={props.required}
-        disabled={props.disabled}
-        title={props.title}
-        value={props.value || (props.defaultValue && props.onChange(props.question, props.defaultValue)) || ''}
-        onChange={(evt) => props.onChange(props.question, evt.target.value)}
-      />
-    </div>
-  );
-}
+class Control extends React.PureComponent {
+  componentDidMount() {
+    if (!this.props.value) {
+      this.props.onChange(this.props.question, this.props.defaultValue || '');
+    }
+  }
 
+  render() {
+    return (
+      <div>
+        <Label>{this.props.question}</Label>
+        <Input
+          type={this.props.type || 'text'}
+          placeholder={this.props.placeholder && this.props.placeholder}
+          required={this.props.required}
+          disabled={this.props.disabled}
+          title={this.props.title}
+          value={this.props.value || ''}
+          onChange={(evt) => this.props.onChange(this.props.question, evt.target.value)}
+        />
+      </div>
+    );
+  }
+}
 Control.propTypes = {
   question: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
