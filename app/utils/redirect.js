876b137b3f5acf9bf7062e1a3aa2b3b8888fa489
Salah Taghzouit <salah.taghzouit@gmail.com>
2016-12-29 16:44:16 +0800

    (fix) better redirect

--- app/utils/redirect.js
+++ app/utils/redirect.js
@@ -1,7 +1,51 @@
-export default (url, redirectBackHere = false) => {
-  let redirect = '';
-  if (redirectBackHere) {
-    redirect = `redirect=${typeof redirectBackHere === 'boolean' ? window.location.href : redirectBackHere}`;
+import isInteger from 'lodash/isInteger';
+import has from 'lodash/has';
+import env from './env';
+
+export default function redirect(url, redirectBackHere = false) {
+  console.log('url is :');
+  console.log(url);
+  if (typeof url !== 'string') {
+    throw new Error('No url to redirect to');
   }
-  window.location.assign(`${url}${redirect ? `?${redirect}` : ''}`);
-};
+
+  let redirectQuery = '';
+  let prefix = '?';
+  console.log(window.location.href);
+  console.log('THAT WAS CURRENT HREF');
+  if (redirectBackHere) {
+    redirectQuery = `redirect=${typeof redirectBackHere === 'boolean' ? window.location.href : redirectBackHere}`;
+    if (url.indexOf('?') > -1) {
+      prefix = '&';
+    }
+  }
+  console.log('redirecting to:');
+  console.log(`${url}${redirectQuery ? `${prefix}${redirectQuery}` : ''}`);
+  window.location.assign(`${url}${redirectQuery ? `${prefix}${redirectQuery}` : ''}`);
+}
+
+/**
+ * Redirect to auth if there's an unauthorized status in error
+ *
+ * @param err
+ * @param redirectBackHere
+ */
+export function redirectToAuthIfUnauth(err, redirectBackHere = true) {
+  let status;
+
+  if (isInteger(err)) {
+    status = err;
+  }
+
+  if (typeof err === 'object') {
+    if (has(err, 'status')) {
+      status = err.status;
+    } else if (has(err, 'response')) {
+      status = err.response.status;
+    }
+  }
+
+  if (status === 401 || status === 400) {
+    redirect(`${env.appUrl}/auth`, redirectBackHere);
+  }
+}
