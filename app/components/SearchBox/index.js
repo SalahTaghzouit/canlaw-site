0c03d18a415794e9ccdadf4b3f2dc314dda77676
Aladin Bouzerd <aladin.bouzerd@gmail.com>
2016-12-29 16:40:40 +0800

    (fix/enhanced) better highlighting for categories results

--- app/components/SearchBox/index.js
+++ app/components/SearchBox/index.js
@@ -1,28 +1,45 @@
 /**
- *
  * SearchBox
- *
  */
 import React from 'react';
 import { connect } from 'react-algoliasearch-helper';
 import { injectIntl } from 'react-intl';
 import messages from './messages';
 
-export function SearchBox({ helper, initialText, intl }) {
-  return (
-    <input
-      placeholder={intl.formatMessage({ ...messages.pleaceholder })}
-      autoFocus
-      defaultValue={initialText}
-      onChange={(e) => helper.setQuery(e.target.value).search()}
-    />
-  );
+class SearchBox extends React.PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.onChangeInput = this.onChangeInput.bind(this);
+  }
+
+  onChangeInput(e) {
+    this.props.onChange(e.target.value);
+    this.props.helper
+      .setQuery(e.target.value)
+      .search();
+  }
+
+  render() {
+    return (
+      <input
+        value={this.props.value}
+        placeholder={this.props.intl.formatMessage({ ...messages.pleaceholder })}
+        autoFocus
+        defaultValue={this.props.initialText}
+        onChange={this.onChangeInput}
+      />
+    );
+  }
 }
+
 
 SearchBox.propTypes = {
   helper: React.PropTypes.object.isRequired,
   initialText: React.PropTypes.string,
   intl: React.PropTypes.object.isRequired,
+  onChange: React.PropTypes.func,
+  value: React.PropTypes.string,
 };
 
 export default connect()(injectIntl(SearchBox));
