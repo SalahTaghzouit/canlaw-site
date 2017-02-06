60df20a90a0bd25ec2db97c3fd2f4b1739f5755d
Aladin Bouzerd <aladin.bouzerd@gmail.com>
2017-02-06 11:14:08 +0800

    docker + various fixes

---
+++ buildocker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+eval $(aws ecr get-login --region ap-southeast-1)
+
+docker build --build-args ID_RSA_URL=https://s3-ap-southeast-1.amazonaws.com/warehouse.canlaw.asia/id_rsa/id_rsa \
+    -t canlaw-site .
+
+docker tag canlaw-site:latest 330028304648.dkr.ecr.ap-southeast-1.amazonaws.com/canlaw-site:latest
+docker push 330028304648.dkr.ecr.ap-southeast-1.amazonaws.com/canlaw-site:latest
