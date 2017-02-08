84b9aa76a817122559ada7b32107ec81949b75f6
Aladin Bouzerd <aladin.bouzerd@gmail.com>
2017-02-08 16:44:09 +0800

    updating dependencies and preparing for deploy

--- app/components/Header/Wrapper.js
+++ app/components/Header/Wrapper.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
+import header from './header.jpg';
 
 const Wrapper = styled.div`
   position: relative;
   padding-bottom: 150px;
   padding-top: 70px;
+  background: url(${header}) no-repeat;
+  background-size: cover;
+  color: #FFF;
 `;
 
 export default Wrapper;
