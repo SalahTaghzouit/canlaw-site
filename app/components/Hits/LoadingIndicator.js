3b257c6df835104b0d98c3185696d6fb9ac1fd1d
Salah Taghzouit <salah.taghzouit@gmail.com>
2017-02-10 09:56:56 +0800

    Loading indicator when loading

---
+++ app/components/Hits/LoadingIndicator.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import BaseLoadingIndicator from 'canlaw-components/components/LoadingIndicator';
+
+const LoadingIndicator = styled(BaseLoadingIndicator)`
+  margin: auto;
+`;
+
+export default LoadingIndicator;
