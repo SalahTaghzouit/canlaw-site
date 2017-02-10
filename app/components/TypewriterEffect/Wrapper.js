638c7acc116842652b551e28c8e8db8f760efec0
Salah Taghzouit <salah.taghzouit@gmail.com>
2017-02-10 09:40:38 +0800

    Better visual cues for typewriting effect

---
+++ app/components/TypewriterEffect/Wrapper.js
@@ -0,0 +1,20 @@
+import styled, { keyframes } from 'styled-components';
+
+const blinkCaret = keyframes`
+  from, to {
+    border-color: transparent;
+  }
+  50% {
+    border-color: white;
+  }
+`;
+
+const Wrapper = styled.span`
+  overflow: hidden; /* Ensures the content is not revealed until the animation */
+  border-right: .05em solid ${(props) => props.color ? props.color : '#FFF'}; /* The typwriter cursor */
+  padding-right: 5px;
+  white-space: nowrap; /* Keeps the content on a single line */
+  animation: ${blinkCaret} .75s step-end infinite;
+`;
+
+export default Wrapper;
