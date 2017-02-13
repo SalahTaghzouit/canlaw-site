a336bf43218dd8f91266f5f06fc1f734d4d2edf0
Salah Taghzouit <salah.taghzouit@gmail.com>
2017-02-13 14:01:33 +0800

    fixed others

--- app/components/Select/index.js
+++ app/components/Select/index.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
-import BaseSelect from 'react-select';
+import { Creatable } from 'react-select';
+
+// import BaseSelect from 'react-select';
 import './scss/default.scss';
 
-const Select = styled(BaseSelect)`
+const Select = styled(Creatable)`
   text-align: left;
   ${(props) => props.hasErrors && `border-color: ${props.theme.danger}!important;`}
 `;
