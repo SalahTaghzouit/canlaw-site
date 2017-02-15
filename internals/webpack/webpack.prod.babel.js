5aaeee441830cdcdd039a946653e973aad384f97
Aladin Bouzerd <aladin.bouzerd@gmail.com>
2017-02-15 18:18:33 +0800

    added GA

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -11,6 +11,8 @@ module.exports = require('./webpack.base.babel')({
   // In production, we skip all hot-reloading stuff
   entry: [
     path.join(process.cwd(), 'app/app.js'),
+    path.join(process.cwd(), 'app/ga.js'),
+
   ],
 
   // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
