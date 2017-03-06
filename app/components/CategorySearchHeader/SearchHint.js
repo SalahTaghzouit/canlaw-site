d5edf59df99e972edafa59258a30d0870bfb67a7
Aladin Bouzerd <aladin.bouzerd@gmail.com>
2017-03-06 16:20:45 +0800

    responsiveness

--- app/components/CategorySearchHeader/SearchHint.js
+++ app/components/CategorySearchHeader/SearchHint.js
@@ -5,6 +5,11 @@ const SearchHint = styled.div`
   text-align: left;
   self-justify: left;
   color: ${(props) => props.theme.lightGray};
+  @media all and (max-width: 768px) {
+    width: 100%;
+    float: none;
+    text-align: center;
+  }
 `;
 
 export default SearchHint;
