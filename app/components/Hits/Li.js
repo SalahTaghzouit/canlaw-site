24b7bd89cc95c03a4fd15a07e97f930688a58fe3
Salah Taghzouit <salah.taghzouit@gmail.com>
2017-03-12 13:23:50 +0800

    Added navigation in search results + search icon

--- app/components/Hits/Li.js
+++ app/components/Hits/Li.js
@@ -1,9 +1,27 @@
 import styled from 'styled-components';
 
 const Li = styled.li`
-    
-  &:hover {
+  position: relative;
+  padding: 10px 0;
+  
+  &:not(:last-child):before {
+    content: "";
+    position: absolute;
+    width: 90%;
+    height: 1px;
+    bottom: 0px;
+    left: 5%;
+    background-color: ${(props) => props.theme.lightestGray};
+    transition: all 0.2s ease-in-out 0s;
+  }
+  
+  ${(props) => props.active && `
+    background: ${props.theme.lightestAccent};
+  `}
+  
+  &:hover, &:active, &:focus {
     background: ${(props) => props.theme.lightestAccent};
+    outline: none;
   }
 `;
 
