b6bd81cab8a7fe3f21545e8f48dafbf05d6abb7c
Julius Ting <jul_ting@yahoo.com>
2017-03-18 16:40:14 +0800

    meet the team component

---
+++ app/components/MeetTheTeam/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import MeetTheTeam from '../index';
+
+describe('<MeetTheTeam />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
