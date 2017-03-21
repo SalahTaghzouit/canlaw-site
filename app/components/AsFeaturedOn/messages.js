acfe9b2a5c5ad5b59c9b152794c2dd504f34488b
Salah Taghzouit <salah.taghzouit@gmail.com>
2017-03-21 11:44:06 +0800

    added AsFeaturedOn

---
+++ app/components/AsFeaturedOn/messages.js
@@ -0,0 +1,29 @@
+/*
+ * AsFeaturedOn Messages
+ *
+ * This contains all the text for the AsFeaturedOn component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'canlaw.site.components.AsFeaturedOn.header',
+    defaultMessage: 'As Featured On',
+  },
+  alp: {
+    id: 'canlaw.site.components.AsFeaturedOn.header',
+    defaultMessage: 'ALP',
+  },
+  timeout: {
+    id: 'canlaw.site.components.AsFeaturedOn.header',
+    defaultMessage: 'Timeout',
+  },
+  officeparrots: {
+    id: 'canlaw.site.components.AsFeaturedOn.header',
+    defaultMessage: 'Office Parrots',
+  },
+  tso: {
+    id: 'canlaw.site.components.AsFeaturedOn.header',
+    defaultMessage: 'The Star Online',
+  },
+});
