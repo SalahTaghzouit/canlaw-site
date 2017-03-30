0d125e1b26b8752953f073eb67201eb711cf7410
Aladin Bouzerd <aladin.bouzerd@gmail.com>
2017-03-30 14:57:31 +0700

    Fixed FAQ page

--- app/components/LawyerBanner/index.js
+++ app/components/LawyerBanner/index.js
@@ -6,38 +6,32 @@
 import React from 'react';
 import { FormattedMessage, injectIntl } from 'react-intl';
 import A from './A';
-import banner from './banner.jpg';
-import Button from './Button';
-import ContainerDiv from './ContainerDiv';
 import H2 from './H2';
 import Header from './Header';
-import Img from './Img';
+import Wrapper from './Wrapper';
 import messages from './messages';
 
-function LawyerBanner({ registerUrl, blogUrl, intl }) {
+function LawyerBanner({ registerUrl, blogUrl }) {
   return (
-    <ContainerDiv>
-      <Img src={banner} alt={intl.formatMessage(messages.growAlt)} />
-      <Header>
-        <FormattedMessage {...messages.grow} />
-      </Header>
-      <Button href={registerUrl}>
-        <FormattedMessage {...messages.signUpLawyer} />
-      </Button>
+    <Wrapper>
+      <Header
+        heading={<FormattedMessage {...messages.grow} />}
+        ctaMessage={<FormattedMessage {...messages.signUpLawyer} />}
+        ctaLink={registerUrl}
+      />
       <H2>
         <FormattedMessage {...messages.nocharge} />
       </H2>
       <A href={`${blogUrl}/pressing-questions-canlaw/`}>
         <FormattedMessage {...messages.readmore} />
       </A>
-    </ContainerDiv>
+    </Wrapper>
   );
 }
 
 LawyerBanner.propTypes = {
   registerUrl: React.PropTypes.string.isRequired,
   blogUrl: React.PropTypes.string.isRequired,
-  intl: React.PropTypes.object.isRequired,
 };
 
 export default injectIntl(LawyerBanner);
