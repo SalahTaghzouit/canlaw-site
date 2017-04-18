ddb1499e7db9311cf28508870647ac23e7543151
Julius Ting <jul_ting@yahoo.com>
2017-04-18 11:36:34 +0800

    updated the team

--- app/components/MeetTheTeam/Img.js
+++ app/components/MeetTheTeam/Img.js
@@ -4,9 +4,7 @@ import styled from 'styled-components';
 const Img = styled(BaseImg)`
   height: 200px;
   border-radius: 50%;
-  border-width: 3px;
-  border-color: ${(props) => props.theme.accent};
-  border-style: solid;
+  
 `;
 
 export default Img;
