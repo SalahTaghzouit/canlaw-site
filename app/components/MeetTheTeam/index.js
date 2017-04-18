fa03dbc42267c96ff9e9777c738ccb13b7fbf261
Salah Taghzouit <salah.taghzouit@gmail.com>
2017-04-18 12:06:26 +0800

    fix warnings and edited to soonyi's pref

--- app/components/MeetTheTeam/index.js
+++ app/components/MeetTheTeam/index.js
@@ -18,6 +18,7 @@ import SoonYi from './assets/soonyi.jpg';
 import GrayArea from './GrayArea';
 import Img from './Img';
 import messages from './messages';
+import StrongWords from './StrongWords';
 
 function MeetTheTeam() {
   return (
@@ -26,49 +27,49 @@ function MeetTheTeam() {
         <FormattedMessage {...messages.header} />
       </AreaHeader>
       <Row alignItems="middle">
-        <Column md="4">
+        <Column md={4}>
           <Img src={SoonYi} alt="Soon Yi" />
-          <p>Loo Soon Yi <br />
+          <p><StrongWords>Loo Soon Yi </StrongWords><br />
             Founder & CEO<br />
             Business Development & Sales</p>
         </Column>
-        <Column md="4">
+        <Column md={4}>
           <Img src={Edwin} alt="Edwin" />
-          <p>Edwin Lee <br />
+          <p><StrongWords>Edwin Lee </StrongWords><br />
             Co-Founder & COO <br />
             Legal & Operations</p>
         </Column>
-        <Column md="4">
+        <Column md={4}>
           <Img src={Amir} alt="Amir" />
-          <p>Amir Rozlan <br />
+          <p><StrongWords>Amir Rozlan </StrongWords><br />
             Co-Founder <br />
             Consultancy</p>
         </Column>
       </Row>
       <Row alignItems="middle">
-        <Column md="4">
+        <Column md={4}>
           <Img src={Aladin} alt="Aladin" />
-          <p>Aladin Bouzerd <br />
+          <p><StrongWords>Aladin Bouzerd </StrongWords><br />
             CTO <br />
             Technology</p>
         </Column>
-        <Column md="4">
+        <Column md={4}>
           <Img src={Jofan} alt="Jofan" />
-          <p>Pang Jo Fan <br />
+          <p><StrongWords>Pang Jo Fan </StrongWords><br />
             Head of Marketing <br />
             Marketing & Communications </p>
         </Column>
-        <Column md="4">
+        <Column md={4}>
           <Img src={Mamta} alt="Mamta" />
-          <p>Mamta Thaker<br />
+          <p><StrongWords>Mamta Thaker </StrongWords><br />
             Executive <br />
             Business Development & Sales</p>
         </Column>
       </Row>
       <Row alignItems="middle">
-        <Column mdShift="4" md="4">
+        <Column mdShift={4} md={4}>
           <Img src={Julius} alt="Julius" />
-          <p>Julius Ting<br />
+          <p><StrongWords>Julius Ting </StrongWords><br />
             Apprentice (Intern) <br />
             Technology</p>
         </Column>
